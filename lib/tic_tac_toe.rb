c11af0a3dce3bb772aeeaf7f4a20fdc48a1a1fda
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-11 20:47:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,6 +61,13 @@ class TicTacToe
 
 
   def turn_count
+    turn = 0
+    @board.each do |index|
+      if index == "X" || index == "O"
+        turn += 1
+      end
+    end
+    return true
   end
 
 end
